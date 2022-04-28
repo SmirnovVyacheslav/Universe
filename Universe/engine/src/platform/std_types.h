5918798f337e863027eb490603849bfebaf4fd71
SmirnovVyacheslav <necrolazy@gmail.com>
2022-04-28 20:33:09 +0300

    Add std types definition

---
+++ Universe/engine/src/platform/std_types.h
@@ -0,0 +1,16 @@
+// Copyright: (C) 2021-2022 Vyacheslav Smirnov. All rights reserved.
+
+#pragma once
+#ifndef STD_TYPES_H
+#define STD_TYPES_H
+
+#include <cstdint>
+
+
+namespace engine
+{
+	using boolen = bool;
+	using integer = int32_t;
+	using real = float;
+}
+#endif
---
+++ Universe/engine/src/platform/stl.h
@@ -0,0 +1,12 @@
+// Copyright: (C) 2021-2022 Vyacheslav Smirnov. All rights reserved.
+
+#pragma once
+#ifndef STL_H
+#define STL_H
+
+
+namespace engine
+{
+	//
+}
+#endif
