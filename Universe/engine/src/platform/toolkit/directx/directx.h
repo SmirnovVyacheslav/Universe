b4319744973cf6ca0946e58370ab5a9793f6f3bb
SmirnovVyacheslav <necrolazy@gmail.com>
2022-05-13 12:43:19 +0300

    Fix issue in directx

--- Universe/engine/src/platform/toolkit/directx/directx.h
+++ Universe/engine/src/platform/toolkit/directx/directx.h
@@ -34,47 +34,6 @@ namespace engine
     };
 
 
-    D3D_DRIVER_TYPE         driverType = D3D_DRIVER_TYPE_HARDWARE;
-    D3D_FEATURE_LEVEL       featureLevel = D3D_FEATURE_LEVEL_11_0;
-
-    ID3D11Device* d3dDevice = nullptr;
-    ID3D11DeviceContext* immediateContext = nullptr;
-    IDXGISwapChain* swapChain = nullptr;
-    ID3D11RenderTargetView* renderTargetView = nullptr;
-
-    ID3D11Texture2D* depthStencil = nullptr;
-    ID3D11DepthStencilView* depthStencilView = nullptr;
-
-    ID3D11InputLayout* vertexLayout = nullptr;
-
-    ID3D11Buffer* constantBuffer = nullptr;
-    ConstantBuffer          localConstantBuffer;
-
-
-    ID3D11Buffer* g_pVertexBuffer = NULL;
-    ID3D11Buffer* g_pIndexBuffer = NULL;
-
-
-    //============Создание поверхности для Z-буфера============
-    ID3D11DepthStencilState* pDSState;
-    D3D11_DEPTH_STENCIL_VIEW_DESC descDSV;
-
-    //================D3D11_RASTERIZER_DESC rasterDesc;==========
-    D3D11_RASTERIZER_DESC rasterDesc;
-    ID3D11RasterizerState* m_rasterState;
-
-    // HWND hWnd;
-    int  wndWidth = 800;
-    int  wndHeight = 600;
-
-    XMMATRIX                g_World;
-    XMMATRIX                g_View;
-    XMMATRIX                g_Projection;
-
-    ID3D11VertexShader* g_pVertexShader = NULL;
-    ID3D11PixelShader* g_pPixelShader = NULL;
-
-
     void createDevice();
     bool createShader();
     bool compileShader(std::wstring path, LPCSTR type, LPCSTR shaderModel, ID3DBlob** blobOut);
