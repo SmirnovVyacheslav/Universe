bded8da35cb9ba3c5b3982ad602da5b780fd4f85
SmirnovVyacheslav <necrolazy@gmail.com>
2022-05-15 19:22:10 +0300

    Add init array and map

---
+++ Universe/engine/src/core/array.h
@@ -0,0 +1,16 @@
+// Copyright: (C) 2021-2022 Vyacheslav Smirnov. All rights reserved.
+
+#pragma once
+#ifndef ARRAY_H
+#define ARRAY_H
+
+
+namespace engine
+{
+    template <class type>
+    class array
+    {
+        //
+    };
+}
+#endif
---
+++ Universe/engine/src/core/map.h
@@ -0,0 +1,16 @@
+// Copyright: (C) 2021-2022 Vyacheslav Smirnov. All rights reserved.
+
+#pragma once
+#ifndef MAP_H
+#define MAP_H
+
+
+namespace engine
+{
+    template <class key, class value>
+    class map
+    {
+        //
+    };
+}
+#endif
