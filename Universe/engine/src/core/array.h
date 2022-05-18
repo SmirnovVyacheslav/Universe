72a875c4656f1e0e9f0dfcef1fc262e898d8222e
SmirnovVyacheslav <necrolazy@gmail.com>
2022-05-18 15:51:17 +0300

    Add array impl

--- Universe/engine/src/core/array.h
+++ Universe/engine/src/core/array.h
@@ -4,13 +4,46 @@
 #ifndef ARRAY_H
 #define ARRAY_H
 
+#include <vector>
+#include <cstdint>
+
 
 namespace engine
 {
     template <class type>
     class array
     {
-        //
+    public:
+        array(std::int32_t size = 0);
+        ~array() = default;
+
+        void append(const type& item);
+
+        type& operator[](const std::int32_t index);
+
+    private:
+        std::vector<type> impl;
     };
+
+
+    template <class type>
+    array<type>::array(std::int32_t size)
+    {
+        impl.reserve(size);
+    }
+
+
+    template <class type>
+    void array<type>::append(const type& item)
+    {
+        impl.push_back(item);
+    }
+
+
+    template <class type>
+    type& array<type>::operator[](const std::int32_t index)
+    {
+        return impl[(impl.size() + index % impl.size()) % impl.size()];
+    }
 }
 #endif
