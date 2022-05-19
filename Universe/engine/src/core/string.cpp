4ce1b15f09a9ce64335d00c91ed5a0d40ac52b47
SmirnovVyacheslav <necrolazy@gmail.com>
2022-05-19 12:21:30 +0300

    Add string convertions

--- Universe/engine/src/core/string.cpp
+++ Universe/engine/src/core/string.cpp
@@ -51,28 +51,6 @@ namespace engine
     }
 
 
-    /*class string_converter
-    {
-    public:
-        static std::string u32_u8(const std::u32string str);
-        static std::u16string u32_u16(const std::u32string str);
-
-    private:
-        string_converter() = default;
-        ~string_converter() = default;
-    };
-
-    std::string string_converter::u32_u8(const std::u32string str)
-    {
-        return std::string();
-    }
-
-    std::u16string string_converter::u32_u16(const std::u32string str)
-    {
-        return std::u16string();
-    }*/
-
-
     string::string(const std::u8string value) :
         id(string_map::inst.add(value))
     {}
@@ -92,12 +70,71 @@ namespace engine
 
     std::u16string string::u16_str()
     {
-        return std::u16string();
+        std::u16string utf16;
+        std::u32string utf32 = u32_str();
+
+        for (std::int32_t i = 0; i < utf32.size(); ++i)
+        {
+            unsigned long unicode = utf32[i];
+            if (unicode <= 0xFFFF)
+            {
+                utf16 += static_cast<char16_t>(unicode);
+            }
+            else
+            {
+                unicode -= 0x10000;
+                utf16 += static_cast<char16_t>((unicode >> 10) + 0xD800);
+                utf16 += static_cast<char16_t>((unicode & 0x3FF) + 0xDC00);
+            }
+        }
+        return utf16;
     }
 
     std::u32string string::u32_str()
     {
-        return std::u32string();
+        std::u32string utf32;
+        std::u8string utf8 = string_map::inst.get(id);
+
+        for (std::int32_t i = 0; i < utf8.length(); ++i) {
+            unsigned char ctrl_char = static_cast<unsigned char>(utf8[i]);
+
+            if (ctrl_char <= 0x7f)
+            {
+                utf32.push_back(utf8[i]);
+            }
+            else if (ctrl_char < 0xe0) {
+                unsigned char a = utf8[i++];
+                utf32.push_back(((a & 0x1f) << 6) + (utf8[i] & 0x3f));
+            }
+            else if (ctrl_char < 0xf0) {
+                unsigned char a = utf8[i++];
+                unsigned char b = utf8[i++];
+                utf32.push_back(((a & 0x0f) << 12) + ((b & 0x3f) << 6) + (utf8[i] & 0x3f));
+            }
+            else if (ctrl_char < 0xf8) {
+                unsigned char a = utf8[i++];
+                unsigned char b = utf8[i++];
+                unsigned char c = utf8[i++];
+                utf32.push_back(((a & 0x07) << 18) + ((b & 0x3f) << 12) + ((c & 0x3f) << 6) + (utf8[i] & 0x3f));
+            }
+            else if (ctrl_char < 0xfc) {
+                unsigned char a = utf8[i++];
+                unsigned char b = utf8[i++];
+                unsigned char c = utf8[i++];
+                unsigned char d = utf8[i++];
+                utf32.push_back(((a & 0x03) << 24) + ((b & 0x3f) << 18) + ((c & 0x3f) << 12) + ((d & 0x3f) << 6) + (utf8[i] & 0x3f));
+            }
+            else if (ctrl_char < 0xfe) {
+                unsigned char a = utf8[i++];
+                unsigned char b = utf8[i++];
+                unsigned char c = utf8[i++];
+                unsigned char d = utf8[i++];
+                unsigned char e = utf8[i++];
+                utf32.push_back(((a & 0x01) << 30) + ((b & 0x3f) << 24) + ((c & 0x3f) << 18) + ((d & 0x3f) << 12) + ((e & 0x3f) << 6) + (utf8[i] & 0x3f));
+            }
+        }
+
+        return utf32;
     }
 
     const char* string::c_str()
