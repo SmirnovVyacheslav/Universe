8c0de8ae3b8e335c9729c4d14033bcdbfbc29566
SmirnovVyacheslav <necrolazy@gmail.com>
2022-05-25 17:59:02 +0300

    Work on methods in memory ptr

--- Universe/engine/src/core/array.h
+++ Universe/engine/src/core/array.h
@@ -2,6 +2,7 @@
 #pragma once
 #include <vector>
 #include <cstdint>
+#include <stdexcept>
 
 
 namespace engine {
@@ -19,8 +20,10 @@ namespace engine {
 
             type_name& operator[](std::int32_t index);
 
+            size_t size() const;
             void append(type_name&& item);
             void append(const type_name& item);
+            std::int32_t find_index(const type_name& item);
         private:
             std::vector<type_name> data;
     };
@@ -35,6 +38,10 @@ namespace engine {
         std::int32_t size = static_cast<std::int32_t>(data.size());
         return data[(size + index % size) % size];
     }
+    template<class type_name>
+    size_t array<type_name>::size() const {
+        return data.size();
+    }
     template <class type_name>
     void array<type_name>::append(type_name&& item) {
         data.push_back(std::move(item));
@@ -42,5 +49,14 @@ namespace engine {
     template <class type_name>
     void array<type_name>::append(const type_name& item) {
         data.push_back(item);
+    }
+    template<class type_name>
+    std::int32_t array<type_name>::find_index(const type_name& item) {
+        for (size_t i = 0; i < data.size(); ++i) {
+            if (data[i] == item) {
+                return static_cast<std::int32_t>(i);
+            }
+        }
+        throw std::invalid_argument("Item does not exist");
     }
 }
--- Universe/engine/src/core/memory.h
+++ Universe/engine/src/core/memory.h
@@ -13,17 +13,22 @@ namespace engine {
     template<class type_name>
     class lead_ptr {
         public:
-            lead_ptr() = default;
-            ~lead_ptr() = default;
+            template<class... type_args>
+            lead_ptr(const type_args&... args);
+            ~lead_ptr();
 
             lead_ptr(lead_ptr&& src) = default;
             lead_ptr(const lead_ptr& src) = delete;
 
+            type_name* operator->();
             lead_ptr& operator=(lead_ptr&& src) = default;
             lead_ptr& operator=(const lead_ptr& src) = delete;
+
+            slave_ptr<type_name>& create_slave_ptr();
+            void destroy_slave_ptr(const slave_ptr<type_name>* ptr);
         private:
             type_name* obj_ptr;
-            array< slave_ptr<type_name> > slave_ptr_list;
+            array< slave_ptr<type_name>* > slave_ptr_list;
     };
     template<class type_name>
     class slave_ptr {
@@ -34,9 +39,55 @@ namespace engine {
             slave_ptr(slave_ptr&& src) = default;
             slave_ptr(const slave_ptr& src) = default;
 
+            type_name* operator->();
             slave_ptr& operator=(slave_ptr&& src) = default;
             slave_ptr& operator=(const slave_ptr& src) = default;
         private:
-            slave_ptr() = default;
+            lead_ptr<type_name>* obj_ptr;
+
+            slave_ptr(const lead_ptr<type_name>* obj_ptr);
     };
+
+
+    template<class type_name>
+    template<class... type_args>
+    lead_ptr<type_name>::lead_ptr(const type_args&... args) {
+        obj_ptr = new type_name(args...);
+    }
+    template<class type_name>
+    lead_ptr<type_name>::~lead_ptr() {
+        for (size_t i = 0; i < slave_ptr_list.size(); ++i) {
+            if (slave_ptr_list[i] != nullptr) {
+                slave_ptr_list[i]->obj_ptr = nullptr;
+            }
+        }
+        delete obj_ptr;
+    }
+    template<class type_name>
+    type_name* lead_ptr<type_name>::operator->() {
+        return obj_ptr;
+    }
+    template<class type_name>
+    slave_ptr<type_name>& lead_ptr<type_name>::create_slave_ptr() {
+        slave_ptr_list.append(new slave_ptr(this));
+        return *slave_ptr_list[-1];
+    }
+    template<class type_name>
+    void lead_ptr<type_name>::destroy_slave_ptr(const slave_ptr<type_name>* ptr) {
+        slave_ptr_list[slave_ptr_list.find_index(ptr)] = nullptr;
+    }
+
+
+    template<class type_name>
+    slave_ptr<type_name>::slave_ptr(const lead_ptr<type_name>* obj_ptr) :
+        obj_ptr(obj_ptr) {
+    }
+    template<class type_name>
+    slave_ptr<type_name>::~slave_ptr() {
+        obj_ptr->destroy_slave_ptr(this);
+    }
+    template<class type_name>
+    type_name* slave_ptr<type_name>::operator->() {
+        return obj_ptr;
+    }
 }
