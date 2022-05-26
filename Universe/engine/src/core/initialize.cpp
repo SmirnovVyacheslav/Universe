491900dafe24992a1053f52ff3ceed56076792db
SmirnovVyacheslav <necrolazy@gmail.com>
2022-05-26 17:11:34 +0300

    Update format in initialize

--- Universe/engine/src/core/initialize.cpp
+++ Universe/engine/src/core/initialize.cpp
@@ -1,19 +1,13 @@
 // Copyright: (C) 2021-2022 Vyacheslav Smirnov. All rights reserved.
-
 #include "initialize.h"
 #include "config.h"
 
 
-namespace engine
-{
-    void initialize()
-    {
+namespace engine {
+    void initialize() {
         config::initialize();
     }
-
-
-    void terminate()
-    {
-
+    void terminate() {
+        //
     }
 }
--- Universe/engine/src/core/initialize.h
+++ Universe/engine/src/core/initialize.h
@@ -1,14 +1,8 @@
 // Copyright: (C) 2021-2022 Vyacheslav Smirnov. All rights reserved.
-
 #pragma once
-#ifndef INITIALIZE_H
-#define INITIALIZE_H
 
 
-namespace engine
-{
+namespace engine {
     void initialize();
-
     void terminate();
 }
-#endif
