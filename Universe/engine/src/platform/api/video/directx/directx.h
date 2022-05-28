89235bde3d2052193667536acb44598e079fd905
SmirnovVyacheslav <necrolazy@gmail.com>
2022-05-28 17:08:19 +0300

    Update format in directx

--- Universe/engine/src/platform/api/video/directx/directx.h
+++ Universe/engine/src/platform/api/video/directx/directx.h
@@ -1,19 +1,12 @@
 ﻿// Copyright: (C) 2021-2022 Vyacheslav Smirnov. All rights reserved.
-#include "src/platform/platform_def.h"
 #pragma once
+#include "src/platform/platform_def.h"
+
 #ifdef WINDOWS
 #include <d3d11.h>
 #include <d3dx11.h>
 #include <d3dcompiler.h>
 #include <windows.h>
-#include <vector>
-#include <memory>
-#include <string>
-#include <unordered_map>
-#include <xnamath.h>
-#include "src/platform/window.h"
-#include "src/resource/mesh.h"
-#include "src/scene/scene.h"
 
 
 namespace engine {
@@ -41,18 +34,5 @@ namespace engine {
             template<class type_name>
             void release_resource(type_name* resource_ptr);
     };
-
-    struct ConstantBuffer {
-        XMMATRIX mWorld;
-        XMMATRIX mView;
-        XMMATRIX mProjection;
-    };
-
-
-    void createDevice();
-    bool createShader();
-    void setGeometry();
-    bool compileShader(std::wstring path, LPCSTR type, LPCSTR shaderModel, ID3DBlob** blobOut);
-    void do_render();
 }
 #endif
