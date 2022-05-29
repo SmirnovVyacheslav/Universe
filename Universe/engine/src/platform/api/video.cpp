8c9a681cf9cdd5b5f81ebb8724aff8996711ef22
SmirnovVyacheslav <necrolazy@gmail.com>
2022-05-29 13:35:04 +0300

    Add simple video init class

---
+++ Universe/engine/src/platform/api/video.cpp
@@ -0,0 +1,9 @@
+// Copyright: (C) 2021-2022 Vyacheslav Smirnov. All rights reserved.
+#include "video.h"
+
+
+namespace engine {
+    void video::create_device() {
+        //
+    }
+}
---
+++ Universe/engine/src/platform/api/video.h
@@ -0,0 +1,15 @@
+// Copyright: (C) 2021-2022 Vyacheslav Smirnov. All rights reserved.
+#pragma once
+
+
+namespace engine {
+    class video {
+    public:
+        video() = default;
+        ~video() = default;
+
+        void create_device();
+    private:
+        //
+    };
+}
