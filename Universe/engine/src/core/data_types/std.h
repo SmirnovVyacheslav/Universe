62a334761c3d5b33dba09ef2f50a444da4aef81f
SmirnovVyacheslav <necrolazy@gmail.com>
2022-07-13 19:26:50 +0300

    Add std type def

---
+++ Universe/engine/src/core/data_types/std.h
@@ -0,0 +1,15 @@
+// Copyright: (C) 2021-2022 Vyacheslav Smirnov. All rights reserved.
+#pragma once
+#include <cstdint>
+
+
+namespace engine {
+    using int_16 = std::int16_t;
+    using int_32 = std::int32_t;
+
+    using uint_16 = std::uint16_t;
+    using uint_32 = std::uint32_t;
+
+    using real_32 = float;
+    using real_64 = double;
+}
