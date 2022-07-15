9749ed510454a1ac89c62ab3d5d89b318511f2aa
SmirnovVyacheslav <necrolazy@gmail.com>
2022-07-15 11:52:08 +0300

    Add initial vector type

---
+++ Universe/engine/src/core/data_types/matrix.h
@@ -0,0 +1,2 @@
+// Copyright: (C) 2021-2022 Vyacheslav Smirnov. All rights reserved.
+#pragma once
---
+++ Universe/engine/src/core/data_types/vector.h
@@ -0,0 +1,28 @@
+// Copyright: (C) 2021-2022 Vyacheslav Smirnov. All rights reserved.
+#pragma once
+#include "std.h"
+
+
+namespace engine {
+    template <uint_32 size>
+    struct vector {
+        real_32[size] data;
+    };
+    union vector_3 {
+        vector<static_cast<uint_32>(3)> data;
+        struct {
+            real_32 x;
+            real_32 y;
+            real_32 z;
+        };
+    };
+    union vector_4 {
+        vector<static_cast<uint_32>(4)> data;
+        struct {
+            real_32 x;
+            real_32 y;
+            real_32 z;
+            real_32 w;
+        };
+    };
+}
