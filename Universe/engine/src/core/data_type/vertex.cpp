e27a78bd608893bae779043ac90f537708a91570
SmirnovVyacheslav <necrolazy@gmail.com>
2022-07-25 18:45:56 +0300

    Add vertex class

---
+++ Universe/engine/src/core/data_type/vertex.cpp
@@ -0,0 +1,9 @@
+// Copyright: (C) 2021-2022 Vyacheslav Smirnov. All rights reserved.
+#include "vertex.h"
+
+
+namespace engine {
+    vertex::vertex(const vector_3& pos, const vector_4& color) :
+        pos(pos), color(color) {
+    }
+}
--- Universe/engine/src/core/data_type/vertex.h
+++ Universe/engine/src/core/data_type/vertex.h
@@ -1,2 +1,22 @@
 // Copyright: (C) 2021-2022 Vyacheslav Smirnov. All rights reserved.
 #pragma once
+#include "vector_3.h"
+#include "vector_4.h"
+
+
+namespace engine {
+    class vertex {
+        public:
+            vertex() = default;
+            vertex(const vector_3& pos, const vector_4& color);
+            ~vertex() = default;
+            vertex(vertex&& src) = default;
+            vertex(const vertex& src) = default;
+
+            vertex& operator=(vertex&& src) = default;
+            vertex& operator=(const vertex& src) = default;
+        private:
+            vector_3 pos;
+            vector_4 color;
+    };
+}
