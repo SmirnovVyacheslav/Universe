6cf5689e32fafad59eb0a4e6c1dfa2bc6594a7d7
SmirnovVyacheslav <necrolazy@gmail.com>
2024-01-16 20:38:27 +0300

    Start move paltform::render to render

---
+++ Universe/engine/src/render/api/device.h
@@ -0,0 +1,18 @@
+// Copyright: (C) 2022 Vyacheslav Smirnov. All rights reserved.
+#pragma once
+#include "src/define/abstract.h"
+
+
+namespace engine::render::device
+{
+    class obj
+    {
+        abstract
+
+        virtual void draw() = 0;
+    };
+
+
+    void init();
+    void term();
+}
---
+++ Universe/engine/src/render/impl/device.cpp
@@ -0,0 +1,21 @@
+// Copyright: (C) 2022 Vyacheslav Smirnov. All rights reserved.
+#pragma once
+#include "src/render/api/device.h"
+
+
+namespace engine::render::device
+{
+    obj* inst = nullptr;
+
+
+    void init()
+    {
+        // inst = new impl;
+    }
+
+    void term()
+    {
+        delete inst;
+        inst = nullptr;
+    }
+}
