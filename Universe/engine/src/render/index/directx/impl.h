3492f671c08991a72660665c3534cf26d9f98b4f
SmirnovVyacheslav <necrolazy@gmail.com>
2024-01-26 19:57:30 +0300

    Work on index

---
+++ Universe/engine/src/render/index/directx/impl.h
@@ -0,0 +1,40 @@
+// Copyright: (C) 2022 Vyacheslav Smirnov. All rights reserved.
+#pragma once
+#include "src/render/index/api.h"
+
+#ifdef platform_windows
+#include <d3d11.h>
+#include <d3dx11.h>
+#include <d3dcompiler.h>
+#include <windows.h>
+#endif
+
+
+namespace engine::render::index::directx
+{
+
+#ifdef platform_windows
+
+    class impl : public obj
+    {
+    public:
+        impl(ID3D11Device* device, uint16* data);
+        impl(impl&& src) = delete;
+        impl(const impl& src) = delete;
+
+        ID3D11Buffer* get_data();
+
+        impl& operator=(impl&& src) = delete;
+        impl& operator=(const impl& src) = delete;
+
+        ~impl();
+    private:
+        uint16* data;
+
+        ID3D11Device* device = nullptr;
+        ID3D11Buffer* index_buffer = nullptr;
+    };
+
+#endif
+
+}
