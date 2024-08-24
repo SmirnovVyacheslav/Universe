450bb9dc793b09e39b1d5906c4274a8033cda874
SmirnovVyacheslav <necrolazy@gmail.com>
2024-08-24 08:21:13 +0300

    Add text impl class

---
+++ Universe/src/engine/render/text/impl_text.cpp
@@ -0,0 +1,12 @@
+// Copyright: (C) 2022 Vyacheslav Smirnov. All rights reserved.
+
+#include "ipf/ips_impl_text.h"
+
+
+namespace engine::render::text
+{
+    void impl::set()
+    {
+        //
+    }
+}
---
+++ Universe/src/engine/render/text/impl_text.h
@@ -0,0 +1,17 @@
+// Copyright: (C) 2022 Vyacheslav Smirnov. All rights reserved.
+
+#include "ipf/iph_impl_text.h"
+
+
+#ifndef ENGINE_RENDER_TEXT_IMPL
+#define ENGINE_RENDER_TEXT_IMPL
+
+namespace engine::render::text
+{
+    class impl : public obj
+    {
+        void set();
+    };
+}
+
+#endif
