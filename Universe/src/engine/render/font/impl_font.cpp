ae844f848b79faac29816fd84e6ce0b5e9e730a8
SmirnovVyacheslav <necrolazy@gmail.com>
2024-09-19 20:24:13 +0300

    Add init font impl

---
+++ Universe/src/engine/render/font/impl_font.cpp
@@ -0,0 +1,9 @@
+// Copyright: (C) 2022 Vyacheslav Smirnov. All rights reserved.
+
+#include "ipf/ips_impl_font.h"
+
+
+namespace engine::render::font
+{
+    //
+}
---
+++ Universe/src/engine/render/font/impl_font.h
@@ -0,0 +1,14 @@
+// Copyright: (C) 2022 Vyacheslav Smirnov. All rights reserved.
+
+#include "ipf/iph_impl_font.h"
+
+
+#ifndef ENGINE_RENDER_FONT_IMPL
+#define ENGINE_RENDER_FONT_IMPL
+
+namespace engine::render::font
+{
+    //
+}
+
+#endif
