87279e62e06683a4541d6dd7efedc2839f83d41e
SmirnovVyacheslav <necrolazy@gmail.com>
2024-09-23 20:26:08 +0300

    Work on font parse

--- Universe/src/engine/render/font/impl_font.cpp
+++ Universe/src/engine/render/font/impl_font.cpp
@@ -11,9 +11,6 @@ namespace engine::render::font
         ifstream.open(settings::get().dir.font.u8_str() + name.u8_str() + u8".fnt");
 
         string str;
-        /*while ((ifstream >> str) && (str != string(u8"index")));
-        if (str != string(u8"index")) {
-            throw error("Incorrect file format");
-        }*/
+        while ((ifstream >> str) && (str.s_str().find("scaleW")));
     }
 }
