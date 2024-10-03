29cfdc344fa747e17ba8e4aba5ecedac11b7fa0d
SmirnovVyacheslav <necrolazy@gmail.com>
2024-10-03 09:51:21 +0300

    Work on char formatting

--- Universe/src/engine/render/font/impl_font.h
+++ Universe/src/engine/render/font/impl_font.h
@@ -2,7 +2,6 @@
 
 #include "ipf/iph_impl_font.h"
 
-
 #ifndef ENGINE_RENDER_FONT_IMPL
 #define ENGINE_RENDER_FONT_IMPL
 
@@ -21,12 +20,19 @@ namespace engine::render::font
 
     class impl : public obj
     {
+    public:
+        impl(string name);
+        impl(impl&& src) = default;
+        impl(const impl& src) = default;
+
+        impl& operator=(impl&& src) = delete;
+        impl& operator=(const impl& src) = delete;
+
+        ~impl() = default;
+    private:
         uint16 tex_width = 0;
         uint16 tex_height = 0;
-
         map<uint16, char_data> char_map;
-
-        impl(string name);
     };
 }
 
