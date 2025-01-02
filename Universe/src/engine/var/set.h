d8d4f6b07a37221e0b32e3625e41597c7303eed3
SmirnovVyacheslav <necrolazy@gmail.com>
2025-01-02 09:40:39 +0300

    Work on set class

---
+++ Universe/src/engine/var/set.h
@@ -0,0 +1,58 @@
+// Copyright: (C) 2022 Vyacheslav Smirnov. All rights reserved.
+
+#include "map.h"
+#include "ptr.h"
+#include "std.h"
+
+#ifndef ENGINE_VAR_SET
+#define ENGINE_VAR_SET
+
+namespace engine
+{
+    template <typename t, typename a1 = int32, typename a2 = int32, typename a3 = int32>
+    class set
+    {
+    public:
+        static int32 add()
+        {
+            //
+        }
+        static int32 add(a1 arg1)
+        {
+            //
+        }
+        static int32 add(a1 arg1, a2 arg2)
+        {
+            //
+        }
+        static int32 add(a1 arg1, a2 arg2, a3 arg3)
+        {
+            //
+        }
+        static ref<t> get(int32 id)
+        {
+            //
+        }
+        static void del(int32 id)
+        {
+            //
+        }
+    private:
+        map<int32, ptr<t>> id_t_map;
+
+        map<int32, int32>                           arg1_id_map;
+        map<int32, map<int32, int32> >              arg2_id_map;
+        map<int32, map<int32, map<int32, int32> > > arg3_id_map;
+
+        set()               = default;
+        set(set&& src)      = delete;
+        set(const set& src) = delete;
+
+        set& operator=(set&& src)      = delete;
+        set& operator=(const set& src) = delete;
+
+        ~set() = default;
+    };
+}
+
+#endif
