09618838bca1dc8a1eba747f83f254a0a7c18bba
SmirnovVyacheslav <necrolazy@gmail.com>
2025-02-06 21:08:12 +0300

    Work on lib/base

--- Universe/src/engine/lib/dt/math/hash.h
+++ Universe/src/engine/lib/dt/math/hash.h
@@ -1,16 +1,7 @@
 // Copyright: (C) 2022 Vyacheslav Smirnov. All rights reserved.
+#pragma once
 
-#include <cstdint>
-#include <functional>
-
-#ifndef ENGINE_STD
-#define ENGINE_STD
-
-namespace engine::lib::dt::base
-{
-    using uint32 = std::uint32_t;
-
-    /*template<>
+/*template<>
     struct hash<uint32>
     {
         size_t operator()(const uint32& arg) const
@@ -18,6 +9,3 @@ namespace engine::lib::dt::base
             return static_cast<size_t>(arg);
         }
     };*/
-}
-
-#endif
