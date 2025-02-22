5929e9ce2c9bc5d696bc9a3a04a8cd932935c8b5
SmirnovVyacheslav <necrolazy@gmail.com>
2025-02-22 10:20:16 +0300

    Add new array

--- Universe/src/engine/lib/datatype/array.h
+++ Universe/src/engine/lib/datatype/array.h
@@ -1,48 +1,75 @@
-//// Copyright: (C) 2022 Vyacheslav Smirnov. All rights reserved.
-//
-//#ifndef ENGINE_LIB_DT_COMP_ARRAY
-//#define ENGINE_LIB_DT_COMP_ARRAY
-//
-//#include <vector>
-//
-//#include "engine/lib/dt/base.h"
-//
-//#include "engine/var/error.h"
-//
-//
-//namespace engine::lib::dt::comp
-//{
-//    template <typename t>
-//    class array
-//    {
-//    public:
-//        array(int32 size = 0)
-//        {
-//            if (size < 0)
-//            {
-//                throw error("Negative array size");
-//            }
-//            data.reserve(static_cast<size_t>(size));
-//        }
-//        array(array&& src) = default;
-//        array(const array& src) = default;
-//
-//        int32 size() const;
-//        void append(T&& item);
-//        void append(const T& item);
-//        int32 find_index(const T& item);
-//
-//        array& operator=(array&& src_ptr) = default;
-//        array& operator=(const array& src_ptr) = default;
-//        T& operator[](const int32 index);
-//        const T& operator[](const int32 index) const;
-//        T* operator*();
-//        const T* operator*() const;
-//
-//        ~array() = default;
-//    private:
-//        std::vector<T> data;
-//    };
-//}
-//
-//#endif
+// Copyright: (C) 2022 Vyacheslav Smirnov. All rights reserved.
+
+#ifndef ENGINE_LIB_DATATYPE_ARRAY
+#define ENGINE_LIB_DATATYPE_ARRAY
+
+#include <vector>
+
+#include "engine/lib/datatype/std.h"
+
+
+namespace engine
+{
+    template <typename t>
+    class array
+    {
+    public:
+        array(uint32 size = 0)
+        {
+            data.reserve(static_cast<size_t>(size));
+        }
+        array(array&& src)      = default;
+        array(const array& src) = default;
+
+        uint32 size() const
+        {
+            return static_cast<uint32>(data.size());
+        }
+        void append(T&& item)
+        {
+            data.push_back(std::move(item));
+        }
+        void append(const T& item)
+        {
+            data.push_back(item);
+        }
+        uint32 find_index(const T& item)
+        {
+            for (uint32 i = 0; i < size(); ++i)
+            {
+                if (data[static_cast<size_t>(i)] == item)
+                {
+                    return i;
+                }
+            }
+            // error
+        }
+
+        array& operator=(array&& src_ptr)      = default;
+        array& operator=(const array& src_ptr) = default;
+
+        t& operator[](const int32 index)
+        {
+            return data[(size() + index % size()) % size()];
+        }
+        const T& operator[](const int32 index) const
+        {
+            return data[(size() + index % size()) % size()];
+        }
+
+        T* operator*()
+        {
+            return &data.front();
+        }
+        const T* operator*() const
+        {
+            return &data.front();
+        }
+
+        ~array() = default;
+    private:
+        std::vector<t> data;
+    };
+}
+
+#endif
